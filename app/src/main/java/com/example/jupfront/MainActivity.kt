dc674532e31595742dce052ff2f11188b1f62ca3
jimin song <songing01@gmail.com>
2024-01-19 22:34:54 +0900

    카카오로그인 테스트

--- app/src/main/java/com/example/jupfront/MainActivity.kt
+++ app/src/main/java/com/example/jupfront/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.jupfront
 
 import android.os.Bundle
+import android.os.Message
 import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private lateinit var webViewLayout: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,26 +22,76 @@ class MainActivity : AppCompatActivity() {
 
         // WebView 초기화
         webView = findViewById(R.id.webview)
+        webViewLayout = findViewById(R.id.webview_frame);
 
+        webView.settings.run {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+            setLoadWithOverviewMode(true) //메타태그 허용
+            setDomStorageEnabled(true) // 로컬저장소 허용
+        }
 
+        //카카오 로그인
+        webView.webChromeClient = object: WebChromeClient() {
 
+            /// ---------- 팝업 열기 ----------
+            /// - 카카오 JavaScript SDK의 로그인 기능은 popup을 이용합니다.
+            /// - window.open() 호출 시 별도 팝업 webview가 생성되어야 합니다.
+            ///
+            override fun onCreateWindow(
+                view: WebView,
+                isDialog: Boolean,
+                isUserGesture: Boolean,
+                resultMsg: Message
+            ): Boolean {
 
-        // WebView 설정
-        val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true // JavaScript 활성화
+                // 웹뷰 만들기
+                var childWebView = WebView(view.context)
 
+                // 부모 웹뷰와 동일하게 웹뷰 설정
+                childWebView.run {
+                    settings.run {
+                        javaScriptEnabled = true
+                        javaScriptCanOpenWindowsAutomatically = true
+                        setSupportMultipleWindows(true)
+                        setLoadWithOverviewMode(true) //메타태그 허용
+                        setDomStorageEnabled(true) // 로컬저장소 허용
+                    }
+                    layoutParams = view.layoutParams
+                    webViewClient = view.webViewClient
+                    webChromeClient = view.webChromeClient
+                }
 
-        webSettings.setJavaScriptEnabled(true) // 웹페이지 자바스클비트 허용 여부
-        webSettings.setSupportMultipleWindows(false) // 새창 띄우기 허용 여부
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(false) // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
-        webSettings.setLoadWithOverviewMode(true) // 메타태그 허용 여부
-        webSettings.setUseWideViewPort(true) // 화면 사이즈 맞추기 허용 여부
-        webSettings.setSupportZoom(false) // 화면 줌 허용 여부
-        webSettings.setBuiltInZoomControls(false) // 화면 확대 축소 허용 여부
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN) // 컨텐츠 사이즈 맞추기
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE) // 브라우저 캐시 허용 여부
-        webSettings.setDomStorageEnabled(true) // 로컬저장소 허용 여부
+                // 화면에 추가하기
+                webViewLayout.addView(childWebView)
+                // TODO: 화면 추가 이외에 onBackPressed() 와 같이
+                //       사용자의 내비게이션 액션 처리를 위해
+                //       별도 웹뷰 관리를 권장함
+                //   ex) childWebViewList.add(childWebView)
 
+                // 웹뷰 간 연동
+                val transport = resultMsg.obj as WebView.WebViewTransport
+                transport.webView = childWebView
+                resultMsg.sendToTarget()
+
+                return true
+            }
+
+            /// ---------- 팝업 닫기 ----------
+            /// - window.close()가 호출되면 앞에서 생성한 팝업 webview를 닫아야 합니다.
+            ///
+            override fun onCloseWindow(window: WebView) {
+                super.onCloseWindow(window)
+
+                // 화면에서 제거하기
+                webViewLayout.removeView(window)
+                // TODO: 화면 제거 이외에 onBackPressed() 와 같이
+                //       사용자의 내비게이션 액션 처리를 위해
+                //       별도 웹뷰 array 관리를 권장함
+                //   ex) childWebViewList.remove(childWebView)
+            }
+        }
 
         // 웹 페이지 로드
         webView.loadUrl("https://lets-jupjup.com")
